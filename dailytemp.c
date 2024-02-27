926b4f289bfa15a1557c38c75765897bd2897c52
Francisco Heleno <119332279+franciscocheleno@users.noreply.github.com>
2024-02-27 09:32:37 +0000

    Create initial dailytemp.c

---
+++ dailytemp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void dtInit(void) 
+{
+	printf("This is a function V2.1");
+	return;
+}
+
+void dtAdd(void) 
+{
+	printf("This is a function V2.1");
+	return;
+}
+void dtStat(void) 
+{
+	printf("This is a function V2.1");
+	return;
+}
+
+void main(void)
+{
+	dtInit();
+  dtAdd();
+	dtStat();
+} 
